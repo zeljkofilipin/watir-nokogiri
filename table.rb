3f734317dff58683893369ca41151e7beac01587
Zeljko Filipin <zeljko.filipin@gmail.com>
2012-05-08 18:51:53 +0200

    find the last cell using it's text, scroll into view and flash it
    nokogiri  0.030000   0.010000   0.040000 (  0.308158)
    watir  0.030000   0.000000   0.030000 (  0.155142)

--- table.rb
+++ table.rb
@@ -7,7 +7,24 @@ browser.goto "file://#{folder}/table.html"
 require "benchmark"
 require "nokogiri"
 
-p Benchmark.measure {Nokogiri::HTML(browser.html).css("td").each {|td| p td.text}}
-p Benchmark.measure {browser.table.tds.each {|td| p td.text}}
+Benchmark.bm do |x|
+  x.report("nokogiri") do
+    Nokogiri::HTML(browser.html).css("td").each do |td|
+      if td.text == "999"
+        css = td.css_path
+        browser.element(css: css).wd.location_once_scrolled_into_view
+        browser.element(css: css).flash
+        break
+      end
+    end
+  end
+end
+
+Benchmark.bm do |x|
+  x.report("watir") do
+    browser.td(text: "999").wd.location_once_scrolled_into_view
+    browser.td(text: "999").flash
+  end
+end
 
 browser.close
