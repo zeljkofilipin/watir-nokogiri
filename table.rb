305bc889044f226699562913b8a52075b63e5bb3
Zeljko Filipin <zeljko.filipin@gmail.com>
2012-05-08 23:17:15 +0200

    get all divs that have ids starting with flightModule

--- table.rb
+++ table.rb
@@ -1,29 +1,20 @@
 require "watir-webdriver"
 browser = Watir::Browser.new :firefox
 
-folder = Dir.pwd
-browser.goto "file://#{folder}/table.html"
+browser.goto "http://www.expedia.com/Flights-Search?trip=roundtrip&leg1=from:SEA,to:SFO,departure:11%2F11%2F2012TANYT&leg2=from:SFO,to:SEA,departure:11%2F16%2F2012TANYT&passengers=children:0,adults:2,seniors:0,infantinlap:Y&options=cabinclass:coach,nopenalty:N,sortby:price&mode=search"
 
 require "benchmark"
 require "nokogiri"
 
 Benchmark.bm do |x|
   x.report("nokogiri") do
-    Nokogiri::HTML(browser.html).css("td").each do |td|
-      if td.text == "999"
-        css = td.css_path
-        browser.element(css: css).wd.location_once_scrolled_into_view
-        browser.element(css: css).flash
-        break
-      end
-    end
+    Nokogiri::HTML(browser.html).css("div[id ^= 'flightModule']").size
   end
 end
 
 Benchmark.bm do |x|
   x.report("watir") do
-    browser.td(text: "999").wd.location_once_scrolled_into_view
-    browser.td(text: "999").flash
+    browser.divs(id: /^flightModule/).size
   end
 end
 
